c02b8af2b893ea0e53af9af2a120b79977c9d78a
Bryan Villamil <bryan.villamil@roiback.com>
2017-11-20 16:29:47 -0500

    cambios en la home se agrego slider

--- home.php
+++ home.php
@@ -1,31 +1,83 @@
 <!DOCTYPE html>
 <html>
     <head>
-  	     <meta charset="UTF-8">
   	     <title>::Bienvenida Kmila::</title>
         <?php include ('inc/head_common.php'); ?>
     </head>
-    <body class="preciosa home">
+    <body class="paginas home">
         <!-- recuadro principal de la pagina -->
         <div class="container">  
-            <div id="slider">
-                <div><img id="1" class="slider-img" src="img/ciudad.jpg"/></div>
-                <div><img class="slider-img" src="img/ciudad2.jpg"/></div>
-                <div><img class="slider-img" src="img/ciudad3.jpg"/></div>
+            <div class="content-top">
+
+                <div class="logo">
+                    <img src="" alt="">
+                </div>
+
+                <div class="manu">
+                    <nav class="nav-bar">
+                        <ul class="menu">
+                            <li class="alpha hover-cycle"><a class="menu1" href="#">Bienvenida kmila a</a></li>
+                            <li class="hover-cycle"><a class="menu2" href="#">Nuestra Historia</a></li>
+                            <li class="hover-cycle"><a class="menu3" href="#">Por fin</a></li>
+                            <li class="hover-cycle"><a class="menu4" href="#">ha llegado el dia...</a></li>
+                            <li class="hover-cycle"><a class="menu5" href="#">No te imaginas cuanto he esperado para esto</a></li>
+                            <li class="hover-cycle"><a class="menu6" href="#">Att: bryan</a></li>     
+                        </ul>
+                    </nav>
+                </div>
             </div>
-            <nav class="nav-bar">
-                <ul class="menu">
-                    <li class="alpha hover-cycle"><a class="menu1" href="#">Bienvenida kmila a</a></li>
-                    <li class="hover-cycle"><a class="menu2" href="#">Nuestra Historia</a></li>
-                    <li class="hover-cycle"><a class="menu3" href="#">Por fin</a></li>
-                    <li class="hover-cycle"><a class="menu4" href="#">ha llegado el dia...</a></li>
-                    <li class="hover-cycle"><a class="menu5" href="#">No te imaginas cuanto he esperado para esto</a></li>
-                    <li class="hover-cycle"><a class="menu6" href="#">Att: bryan</a></li>     
-                </ul>
-            </nav>
+            <div class="content-bottom">
+                <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                    </ol>
+
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <div class="dark">
+                                <h1 contenteditable="true">Chicago</h1>
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <div class="light">
+                                <h1 contenteditable="true">Houston</h1>
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <div class="dark">
+                                <h1 contenteditable="true">New York</h1>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Left and right controls -->
+                    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div> 
+            </div>
         </div>
 
-        <!--fin de cuadro de imagenes de premios de base de datos-->
+
+        <div class="box-text">
+            <div class="descripcion">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia iste, in delectus error, ad minus consectetur excepturi sint quam aspernatur, magnam quas nemo numquam modi quos? Necessitatibus sapiente sit quam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam amet repudiandae perferendis aperiam magni reiciendis recusandae accusamus repellat. Quisquam optio consectetur placeat libero deleniti, animi eius quae laborum necessitatibus repudiandae.</p>
+            </div>
+        </div>
+
+
+        <!-- botons -->
         <div class="buttons">
             <a href="pagina1.php" class="btn-sig btn-primary">
                 <img src="img/chulo.png" alt=""/>
@@ -33,20 +85,9 @@
             </a>
         </div> 
 
-        <!--pie de pagina con nombre y año-->
+        <!-- include FOOTER -->
         <?php include 'inc/footer_common.php'; ?>
 
-        <script type="text/javascript">
-            // pagina de home
-            //------ imagenes de slider rapidas con gran fondo excelente
-            $(function(){
-                $('#slider div:gt(0)').hide();
-                setInterval(function(){
-                $('#slider div:first-child').fadeOut(0)
-                   .next('div').fadeIn(0)
-                   .end().appendTo('#slider');}, 300);
-            });
-        </script>
     </body>
 </html>
 
