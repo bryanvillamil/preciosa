22352cf3aa11e6b390dd90fc724da51a09b84c0d
Bryan <bryan.villamil@roiback.com>
2018-01-10 13:58:13 -0500

    se agrego nuevo menu mobil

--- inc/menuMobile.php
+++ inc/menuMobile.php
@@ -1,53 +1,57 @@
 <div class="nav-mobile">
-    <div class="navbar__close">
-        <i class="fa fa-times"></i>
-    </div>
-    <div class="titulo">
-        <h2 class="titleMenu">Menu</h2>
-    </div>
-    <div class="logo">
-        <!--<img src="./styles/img/nombres/cami16.jpg" alt="">-->
-    </div>
     <div class="menu">
+        <div class="navbar__close">
+            <i class="fa fa-times"></i>
+        </div>
+        <!--
+        <div class="titulo">
+            <h2 class="titleMenu">Menu</h2>
+        </div>
+        <div class="logo">
+            <!--<img src="./styles/img/nombres/cami16.jpg" alt="">
+        </div>
+        -->
         <nav class="menu-mobil">
             <ul>
-                <li><a href="./inicio.php" itemprop="url">Inicio <i class="fa fa-heart" aria-hidden="true"></i></a></li>
-                    <li><a href="./home.php" itemprop="url">Home <i class="fa fa-home" aria-hidden="true"></i></a></li>
+                <li><a href="./inicio.php" itemprop="url"><i class="fa fa-heart" aria-hidden="true"></i> Inicio</a></li>
+                <li><a href="./home.php" itemprop="url"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
+                <li><a href="./pagina1.php" itemprop="url"><i class="fa fa-address-book" aria-hidden="true"></i> Nuestra Historia</a></li>
+            <!--
                 <li>
                     <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
                         <div class="panel panel-default">
                             <div class="panel-heading" role="tab" id="headingOne">
                                 <h4 class="panel-title">
-                                    <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Paginas<span class="caret"></span>
+                                    <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Nuestra Historia<span class="caret"></span>
                                     </a>
                                 </h4>
                             </div>
                             <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
                                 <div class="panel-body">
-                                    <a href="./pagina1.php">página 1</a>
-                                    <a href="./pagina2.php">página 2</a>
-                                    <a href="./pagina3.php">página 3</a>
-                                    <a href="./pagina4.php">página 4</a>
-                                    <a href="./pagina5.php">página 5</a>
-                                    <a href="./pagina6.php">página 6</a>
-                                    <a href="./pagina7.php">página 7</a>
-                                    <a href="./pagina8.php">página 8</a>
-                                    <a href="./pagina9.php">página 9</a>
-                                    <a href="./pagina10.php">página 10</a>
-                                    <a href="./pagina11.php">página 11</a>
-                                    <a href="./pagina12.php">página 12</a>
-                                    <a href="./pagina13.php">página 13</a>
-                                    <a href="./pagina14.php">página 14</a>
-                                    <a href="./pagina15.php">página 15</a>
+                                    <a href="./pagina1.php">Nuestro Inicio</a>
+                                    <a href="./pagina2.php">O Èl o Yo</a>
+                                    <a href="./pagina3.php">Saliendo Contigo Parte 1</a>
+                                    <a href="./pagina4.php">Mis Primeros Enojos</a>
+                                    <a href="./pagina5.php">Una Gran Noche</a>
+                                    <a href="./pagina6.php">Odio Verte Llorar</a>
+                                    <a href="./pagina7.php">Lo Mejor Es...</a>
+                                    <a href="./pagina8.php">Casi Me Da Un Infarto</a>
+                                    <a href="./pagina9.php">Creo Que Te Embarre Jejeje</a>
+                                    <a href="./pagina10.php">Saliendo Contigo Parte 2</a>
+                                    <a href="./pagina11.php">Te Mudaste</a>
+                                    <a href="./pagina12.php">Solo Te Amo A Ti</a>
+                                    <a href="./pagina13.php">Conociendo Tu Mansion</a>
+                                    <a href="./pagina14.php">Estas Preciosa</a>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </li>
-                <li><a href="./pagina15.php">Nuestro album <i class="fa fa-camera-retro" aria-hidden="true"></i></a></li>
-                <li><a href="./pagina16.php">Juguemos <i class="fa fa-puzzle-piece" aria-hidden="true"></i></a></li>
-                <li><a href="./pagina17.php">!00 Razones <i class="fa fa-list-ol" aria-hidden="true"></i></a></li>
-                <li class="salir"><a href="./index.php">salir<i class="fa fa-sign-in" aria-hidden="true"></i></a></li>
+            -->
+                <li><a href="./pagina15.php"><i class="fa fa-camera-retro" aria-hidden="true"></i> Nuestro album</a></li>
+                <li><a href="./pagina16.php"><i class="fa fa-puzzle-piece" aria-hidden="true"></i>Juguemos </a></li>
+                <li><a href="./pagina17.php"><i class="fa fa-list-ol" aria-hidden="true"></i> !00 Razones</a></li>
+                <li class="salir"><a href="./index.php"><i class="fa fa-sign-in" aria-hidden="true"></i> salir</a></li>
             </ul>
         </nav>
     </div>
