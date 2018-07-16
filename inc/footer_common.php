870c6dd91e9bd0f288a020da98b7858ed3cb9820
bryan-villa1000 <bryanvillamilacevedo@gmail.com>
2018-07-15 20:28:29 -0500

    text footer

--- inc/footer_common.php
+++ inc/footer_common.php
@@ -1,7 +1,7 @@
 <!--pie de pagina-->
 <footer>
     <div class="info">
-    	<p>Juntos a ti NO le temo a nada</p>
+    	<p>Juntos, NO le temo a nada</p>
         <p>BrYaN ViLlaMiL AcEvEdO ® 2018</p>
     </div>
 </footer>
